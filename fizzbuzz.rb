8f0254215bd3b5e7ef0a7ae7d92295fbc279ae43
Sabrina Chowdhury <nebula155@gmail.com>
2019-01-29 17:52:03 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,12 +2,12 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(int)
-  if int % 3 == 0
-    "Fizz"
+  if int % 5 == 0 && int % 3 == 0
+    "FizzBuzz"
   elsif int % 5 == 0
     "Buzz"
-  elsif int % 5 == 0 && int % 3 == 0
-    "FizzBuzz"  
+  elsif int % 3 == 0
+    "Fizz"  
   else 
     return NIL
   end  
